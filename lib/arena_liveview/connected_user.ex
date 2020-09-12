cca1b8edd8c2699847174842c652490b21dcd6ec
Pehuén <pehuen.rodriguez@nayra.coop>
2020-09-12 14:09:21 -0300

    Move connected_user creation to its own module

--- lib/arena_liveview/connected_user.ex
+++ lib/arena_liveview/connected_user.ex
@@ -1,3 +1,8 @@
 defmodule ArenaLiveview.ConnectedUser do
   defstruct uuid: ""
+  alias ArenaLiveview.ConnectedUser
+
+  def create_connected_user do
+    %ConnectedUser{uuid: UUID.uuid4()}
+  end
 end
